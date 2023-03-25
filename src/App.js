20a94d202d02c01a44fcda7e1f508d9ebb52a3e1
Tien Kim <tienkim9920@gmail.com>
2023-03-25 11:40:29 +0700

    Lesson - 5

--- src/App.js
+++ src/App.js
@@ -1,41 +1,12 @@
 import './App.css';
+import Home from './Home';
+import Navbar from './Navbar';
 
 function App() {
-  const nineDev = 'Nine Dev';
-  const arrayNineDev = [0, 1 , 2 , 3, 4];
-  const a = 3;
-
-  const arrayMap = [
-    {
-      name: 'Nine Dev',
-      code: 'Frontend Dev'
-    },
-    {
-      name: 'Nine Dev 002',
-      code: 'Frontend Dev'
-    },
-    {
-      name: 'Nine Dev 003',
-      code: 'Frontend Dev'
-    },
-  ]
-
-  const google = 'https://www.google.com'
-
   return (
     <div className="App">
-      <h1>{nineDev}</h1>
-      <div>{arrayNineDev}</div>
-      <div>{a}</div>
-      <div>
-        {arrayMap.map((item, index) => (
-          <div key={index}>
-              <div>{item.name} - {index}</div>
-              <div>{item.code}</div>
-          </div>
-        ))}
-      </div>
-      <a href={google}>Google</a>
+      <Navbar />
+      <Home />
     </div>
   );
 }
