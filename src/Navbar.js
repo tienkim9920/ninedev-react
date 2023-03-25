dc9d3761bb36a29655f71c91eefdcf3653334ab5
Tien Kim <tienkim9920@gmail.com>
2023-03-25 15:35:24 +0700

    update

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,11 @@ const Navbar = () => {
             <h1>Nine Dev</h1>
             <div className="links">
                 <a href="/">Home</a>
-                <a href="/create">New Blog</a>
+                <a href="/create" style={{
+                    color: 'white',
+                    background: '#003A57',
+                    borderRadius: '8px'
+                }}>New Blog</a>
             </div>
         </nav>
     )
