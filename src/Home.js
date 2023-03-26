a653ef226c588f78859522f613e5f68052dd6aa1
Tien Kim <tienkim9920@gmail.com>
2023-03-26 11:06:00 +0700

    lesson-10

--- src/Home.js
+++ src/Home.js
@@ -2,22 +2,23 @@ import { useState } from "react";
 
 const Home = () => {
 
-    const [name, setName] = useState('Nguyen Van A');
-    const [age, setAge] = useState(25);
-    const [address, setAddress] = useState('');
-
-    const handleClickEvent = () => {
-        setName('Nine Dev');
-        setAge(30);
-        setAddress('TP. HCM');
-        console.log("Nine Dev")
-    }
+    const [blogs, setBlogs] = useState([
+        { title: 'Running Man', body: 'Lorem ....', author: 'Nine Dev', id: '1' },
+        { title: 'Running Women', body: 'Lorem ....', author: 'Nine Dev', id: '2' },
+        { title: 'Hello world', body: 'Lorem ....', author: 'Nine Dev', id: '3' },
+    ])
 
     return (
         <div>
             <h1>Home Page</h1>
-            <div>{name} - {age} old - {address}</div>
-            <button onClick={handleClickEvent}>Click Event</button>
+            {
+                blogs.length && blogs.map(item => (
+                    <div key={item.id} className="blog-preview">
+                        <h2>{item.title}</h2>
+                        <div>{item.body}</div>
+                    </div>
+                ))
+            }
         </div>
     )
 }
