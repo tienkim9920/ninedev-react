87f9cd8624b4163bfb2b647f7b4253e844dca8cc
Tien Kim <tienkim9920@gmail.com>
2023-03-26 10:46:21 +0700

    lesson-8

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,23 @@
+import { useState } from "react";
+
 const Home = () => {
 
-    const handleClickEvent = () => {
-        console.log("Nine Dev")
-    }
+    const [name, setName] = useState('Nguyen Van A');
+    const [age, setAge] = useState(25);
+    const [address, setAddress] = useState('');
 
-    const handleClickEvents = (value) => {
-        console.log(value);
+    const handleClickEvent = () => {
+        setName('Nine Dev');
+        setAge(30);
+        setAddress('TP. HCM');
+        console.log("Nine Dev")
     }
 
     return (
         <div>
             <h1>Home Page</h1>
+            <div>{name} - {age} old - {address}</div>
             <button onClick={handleClickEvent}>Click Event</button>
-            <button onClick={() => handleClickEvents("Click Events")}>Click Events</button>
         </div>
     )
 }
