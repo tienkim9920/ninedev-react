df6c1ad24314c434d61d9bcfa2628110f5641f2d
Tien Kim <tienkim9920@gmail.com>
2023-03-26 10:17:23 +0700

    lesson-7

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+    const handleClickEvent = () => {
+        console.log("Nine Dev")
+    }
+
+    const handleClickEvents = (value) => {
+        console.log(value);
+    }
+
     return (
         <div>
             <h1>Home Page</h1>
+            <button onClick={handleClickEvent}>Click Event</button>
+            <button onClick={() => handleClickEvents("Click Events")}>Click Events</button>
         </div>
     )
 }
