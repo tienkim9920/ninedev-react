59f586ee4ecdf244f373e04e5435a21ab6e2f71a
Tien Kim <tienkim9920@gmail.com>
2023-04-18 18:29:53 +0700

    update

--- src/Home.js
+++ src/Home.js
@@ -1,33 +1,30 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState([
-        { title: 'Running Man', body: 'Lorem ....', author: 'mario', id: '1' },
-        { title: 'Running Women', body: 'Lorem ....', author: 'mario', id: '2' },
-        { title: 'Hello world', body: 'Lorem ....', author: 'yoshi', id: '3' },
-        { title: 'Hello Develop', body: 'Lorem ....', author: 'yoshi', id: '4' },
-    ])
+    const [blogs, setBlogs] = useState([]);
 
-    const handleDeleteBlog = (id, title) => {
-        console.log(title);
+    const handleDeleteBlog = (id) => {
         setBlogs(blogs.filter(item => item.id !== id));
     }
 
-    const [show, setShow] = useState(false);
-
     useEffect(() => {
-        console.log("Home Page");
-    }, [show])
+        (async () => {
+            const res = await fetch('http://localhost:8000/blogs');
+            const { data } = await res.json();
+            if (data?.length > 0) {
+                setBlogs(data);
+            }
+        })();
+    }, [])
 
     return (
         <div>
             {
-                show && 
+                blogs.length && 
                 <BlogList handleDeleteBlog={handleDeleteBlog} blogs={blogs} author="Nine Dev Blog"/>
             }
-            <button onClick={() => setShow(true)}>Show Blog List</button>
         </div>
     )
 }
