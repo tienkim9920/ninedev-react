369792b86dd02e42f3cba0d6bc5f2391a5dc6115
Tien Kim <tienkim9920@gmail.com>
2023-04-25 19:41:11 +0700

    update

--- src/BlogDetail.js
+++ src/BlogDetail.js
@@ -1,13 +1,27 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const BlogDetail = () => {
-    const { id } = useParams();
-    
-    return (
+  const { id } = useParams();
+  const {
+    data: blog,
+    isLoading,
+    isError,
+  } = useFetch(`http://localhost:8000/blogs/${id}`);
+
+  return (
+    <div>
+      {isLoading && <div>Loading ...</div>}
+      {isError && <div>Server Internal Error</div>}
+      {blog && (
         <div>
-            <h1>Blog Detail - {id}</h1>
+          <h1>{blog?.title}</h1>
+          <h4>{blog?.author}</h4>
+          <div>{blog?.body}</div>
         </div>
-    )
+      )}
+    </div>
+  );
 };
 
 export default BlogDetail;
