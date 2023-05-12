c4e80015222145880ccc8e8fd3eed3b003a2b2ca
Tien Kim <tienkim9920@gmail.com>
2023-05-12 17:52:22 +0700

    update

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,47 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const Create = () => {
+    const [blog, setBlog] = useState({});
+    const history = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!blog.title || !blog.body || !blog.author) return;
+
+        (async () => {
+            const body = {id: Math.random(), ...blog};
+            const res = await fetch('http://localhost:8000/blogs', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(body)
+            })
+            const { data, status } = await res.json();
+            if (status) {
+                console.log(data);
+                history('/');
+            }
+        })();
+    }
+
     return (
-        <div>
-            <h1>Add New Blog For NineDev</h1>
+        <div className="create">
+            <h1>Add New Blog</h1>
+            <form onSubmit={handleSubmit}>
+                <label>Title</label>
+                <input type="text" value={blog.title || ''} onChange={(e) => setBlog({...blog, ['title']: e.target.value})} />
+                <label>Body</label>
+                <textarea type="text" value={blog.body || ''} onChange={(e) => setBlog({...blog, ['body']: e.target.value})}></textarea>
+                <label>Author</label>
+                <select value={blog.author || ''} onChange={(e) => setBlog({...blog, ['author']: e.target.value})}>
+                    <option value=''>Choose Author</option>
+                    <option value="mario">Mario</option>
+                    <option value="yoshi">Yoshi</option>
+                </select>
+                <button disabled={!blog.title || !blog.body || !blog.author}>Add Blog</button>
+            </form>
         </div>
     )
 }
